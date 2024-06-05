9b9762a6b988a335b0bef0d00c598dfcce1787e2
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-05 16:07:18 +0200

    Add css + function button + innerHTML

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let invio = document.querySelector(".genera");
 invio.addEventListener("click",
     function(){
         let nameUser = document.querySelector(".user-name").value;
+        document.getElementById("name-pass").innerHTML = nameUser;
         console.log(nameUser);
 
         let ageUser = document.querySelector(".age-user").value;
@@ -23,15 +24,44 @@ invio.addEventListener("click",
     
             priceTicket = (priceTicket - (priceTicket / 100 * 20));
             console.log(priceTicket);
-        
+            document.getElementById("type-ticket").innerHTML = "Ticket Under";
+
         } else if (ageUser === "Over 65"){ // - va applicato uno sconto del 40% per gli over 65.
         
             priceTicket = (priceTicket - (priceTicket / 100 * 40));
             console.log(priceTicket);
+            document.getElementById("type-ticket").innerHTML = "Ticket Over";
+
+        } else {
+
+            document.getElementById("type-ticket").innerHTML = "Ticket Standard";
+
         }
 
+
+        // Il recap dei dati e l’output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
         let finalPriceTicket = Math.round(priceTicket * 100) / 100;
         console.log(finalPriceTicket);
+
+        let messagePrice = document.querySelector("#price-ticket")
+        messagePrice.innerHTML = finalPriceTicket 
+
+        // Quando "clicchiamo" genera si crea un biglietto virtuale
+
+        let fakeTicket = document.querySelector(".ticket");
+        fakeTicket.classList.add("active")
+        console.log(fakeTicket);
+
+        // Quando "clicchiamo" annulla si cancella il biglietto virtuale
+        let cancel = document.querySelector(".annulla")
+        cancel.addEventListener("click",
+            function(){
+                fakeTicket.classList.remove("active")
+            }
+        );
+
+
+
     }
 );
 
